06e6e2a5589e356a63c04f7ee32a346fadf5f21d
Anna Sudol <sudolann@gmail.com>
2024-11-07 18:51:24 +0100

    top nav

--- app/components/global/IconTopMenu.vue
+++ app/components/global/IconTopMenu.vue
@@ -10,36 +10,74 @@ const isMobileOpenClass = ref('flex-row')
 </script>
 
 <template>
-  <div class="ltablet:flex grow justify-center gap-x-6 lg:flex">
+  <div>
     <div
-      v-for="(item, index) in menuItems"
-      :key="index"
-      class="text-center"
+      class="hidden md:flex grow justify-center gap-x-6"
     >
-      <button
-        v-if="item.children"
-        type="button"
-        :class="[{ [isSelectedClass]: selectedMenuItem?.activePath === item.activePath }, { [isMobileOpenClass]: [props.open] }, { ['flex flex-col']: [!props.open] }]"
-
-        @click="
-          () => {
-            selectedMenuItem = item,
-            router.push(item.children?.[0]?.to)
-          }
-        "
+      <div
+        v-for="(item, index) in menuItems"
+        :key="index"
+        class="text-center"
       >
-        <div
-          class="dark:text-muted-300 text-gray-700 group-hover:bg-muted-100 group-hover:text-muted-600 dark:group-hover:bg-muted-800 dark:group-hover:text-muted-200 relative mx-auto flex size-7 items-center justify-center rounded-xl transition-colors duration-300"
+        <button
+          v-if="item.children"
+          type="button"
+          :class="[{ [isSelectedClass]: selectedMenuItem?.activePath === item.activePath }, { [isMobileOpenClass]: [props.open] }, { ['flex flex-col']: [!props.open] }]"
+
+          @click="
+            () => {
+              selectedMenuItem = item,
+              router.push(item.children?.[0]?.to)
+            }
+          "
         >
-          <Icon
-            :name="item.icon.name"
-            class="w-5 h-5"
-          />
-        </div>
-        <p class="dark:text-muted-300 text-gray-700 text-center font-sans text-xs">
-          {{ item.name }}
-        </p>
-      </button>
+          <div
+            class="dark:text-muted-300 text-gray-700 group-hover:bg-muted-100 group-hover:text-muted-600 dark:group-hover:bg-muted-800 dark:group-hover:text-muted-200 relative mx-auto flex size-7 items-center justify-center rounded-xl transition-colors duration-300"
+          >
+            <Icon
+              :name="item.icon.name"
+              class="w-5 h-5"
+            />
+          </div>
+          <p class="dark:text-muted-300 text-gray-700 text-center font-sans text-xs">
+            {{ item.name }}
+          </p>
+        </button>
+      </div>
+    </div>
+    <div
+      class="block md:hidden"
+      :class="!open && 'hidden'"
+    >
+      <div
+        v-for="(item, index) in menuItems"
+        :key="index"
+        class="text-center"
+      >
+        <button
+          v-if="item.children"
+          type="button"
+          class="flex flex-row items-center my-3"
+          @click="
+            () => {
+              selectedMenuItem = item,
+              router.push(item.children?.[0]?.to)
+            }
+          "
+        >
+          <div
+            class="dark:text-muted-300 text-gray-700 group-hover:bg-muted-100 group-hover:text-muted-600 dark:group-hover:bg-muted-800 dark:group-hover:text-muted-200 relative mx-auto flex size-7 items-center justify-center rounded-xl transition-colors duration-300"
+          >
+            <Icon
+              :name="item.icon.name"
+              class="w-5 h-5"
+            />
+          </div>
+          <p class="dark:text-muted-300 text-gray-700 text-center font-sans text-xs ml-2">
+            {{ item.name }}
+          </p>
+        </button>
+      </div>
     </div>
   </div>
 </template>
