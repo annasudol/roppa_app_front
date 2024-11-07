714998beb9f121092a8609407d7eb83101315e4a
Anna Sudol <sudolann@gmail.com>
2024-11-07 18:22:05 +0100

    icon top

--- app/components/global/IconTopMenu.vue
+++ app/components/global/IconTopMenu.vue
@@ -29,14 +29,14 @@ const isMobileOpenClass = ref('flex-row')
         "
       >
         <div
-          class="text-muted-400 group-hover:bg-muted-100 group-hover:text-muted-600 dark:group-hover:bg-muted-800 dark:group-hover:text-muted-300 relative mx-auto flex size-11 items-center justify-center rounded-xl transition-colors duration-300"
+          class="dark:text-muted-300 text-gray-700 group-hover:bg-muted-100 group-hover:text-muted-600 dark:group-hover:bg-muted-800 dark:group-hover:text-muted-200 relative mx-auto flex size-11 items-center justify-center rounded-xl transition-colors duration-300"
         >
           <Icon
             :name="item.icon.name"
             :class="item.icon.class"
           />
         </div>
-        <p class="text-muted-400 w-full text-center font-sans text-xs">
+        <p class="dark:text-muted-300 text-gray-700 w-full text-center font-sans text-xs">
           {{ item.name }}
         </p>
       </button>
