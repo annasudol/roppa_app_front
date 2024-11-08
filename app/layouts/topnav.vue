dd2c27103b43ce53c8fe1abda74ece6d2ee44816
Anna Sudol <sudolann@gmail.com>
2024-11-08 10:08:17 +0100

    removed l;ayout

--- app/layouts/topnav.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import TairoTopnavLayout from '~/components/tairo/TairoTopnavLayout.vue'
-</script>
-
-<template>
-  <TairoTopnavLayout>
-    <slot />
-  </TairoTopnavLayout>
-</template>
